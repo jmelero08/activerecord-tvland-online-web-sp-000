1bad591d9c29f4fcf0530ef216eba6bb2b3337e5
Jose Melero Martinez <jmelero08@gmail.com>
2020-12-30 18:03:11 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,7 +1,9 @@
-class Network < ActiveRecord::Base
-  has_many :shows
+class Show < ActiveRecord::Base
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
 
-  def sorry
-    "We're sorry about passing on John Mulaney's pilot"
+  def actors_list
+    self.actors.map{|actor| actor.full_name}
   end
 end
